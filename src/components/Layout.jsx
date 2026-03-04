5a2c25fcc044863e4489f76a0accf5c952c7ac03
Justin Hutzler <hootzluh@gmail.com>
2026-03-04 14:28:12 -0600

    chore: bump version to 2.3.0

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { openHelpWindow } from '../lib/helpWindow';
 import { checkForUpdate, downloadAndInstallUpdate } from '../lib/appUpdater';
 
-const APP_VERSION = '2.2.4';
+const APP_VERSION = '2.3.0';
 
 function Layout({ children }) {
   const location = useLocation();
